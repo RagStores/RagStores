50661560b7c94a01a1f9ed702f0c2d55ec4f2b29
LeonardoFeriato <lmferiato@gmail.com>
2021-01-01 23:56:10 -0300

    Version 3.0

---
+++ js/util/jquery.inputsenter.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function($) {
+    // Remove "Enter" ou Selection dependence 
+    ////var controller = true;
+    $(".flexdatalist-multiple").focusin(function() {
+
+        //console.log($(this).find("input").attr("id"));
+
+        // Split the string and use only the first piece
+        // All input id is like: txtInput-flexdatalist
+        var txtFullName = $(this).find("input").attr("id");
+        var txtName = txtFullName.split('-');
+
+        // Restrict elements
+        if (txtFullName != "txtRefine-flexdatalist" &&
+        txtFullName != "txtItens-flexdatalist" &&
+        txtFullName != "flexdatalist-txtCards" &&
+        txtFullName != "flexdatalist-txtEnchants" &&
+        txtFullName != "s-flexdatalist") {
+
+            // Lost focus
+            $(".flexdatalist-multiple").focusout(function() {
+
+                // Check if the Controller is ON and if the input has no value
+                if (!$("#"+txtName[0]).val()) { // Value = NULL or FALSE
+                    ////if (controller && $("#txtAccountSocial").val()) {  
+
+                    $("#"+txtName[0]).val(
+                        // Set the value you entered in the input
+                        $("#"+txtFullName).val()
+                    );
+                    ////controller = false;
+
+                } else { // For some reason without this code below, to click on X doesn't work to remove value
+
+                    // Use the Remove Method to clear the entered value 
+                    $("#"+txtName[0]).flexdatalist(
+                        "remove",
+                        $("#"+txtName[0]).val()
+                    );
+                    ////controller = true;
+                }
+
+            });
+        }
+    });
+})
